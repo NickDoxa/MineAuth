87bd0b676e099a62e884b4f8bf385cd725541e94
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-08-30 15:11:12 -0400

    Added login form component

---
+++ mineauth-frontend/src/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Banner from "@/components/Banner.vue";
+
+</script>
+
+<template>
+  <Banner/>
+  <div class="centered-container">
+    <h1>Login</h1>
+    <form>
+      <label>Username</label><br>
+      <input type="text" placeholder="Username..."><br><br>
+      <label>Password</label><br>
+      <input type="password" placeholder="Password..."><br>
+      <button style="margin:10px"  id="submit-btn"><b>Login</b></button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.centered-container {
+  display: block;
+  position: relative;
+  text-align: center;
+  height: 30%;
+  width: 30%;
+  border: 2px solid black;
+  border-radius: 10px;
+  padding: 5px;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  background-color: white;
+}
+
+#submit-btn {
+  background-color: lightslategray;
+  color: #1D1D1D;
+  width: 100px;
+  height: 30px;
+  font-size: large;
+}
+</style>
