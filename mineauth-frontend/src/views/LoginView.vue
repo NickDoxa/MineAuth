279051e05603e77988a19bd794aad8b3278f3879
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-08-31 21:23:14 -0400

    added new views

---
+++ mineauth-frontend/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LoginForm from "@/components/LoginForm.vue";
+import Banner from "@/components/Banner.vue";
+import NotFound from "@/views/NotFound.vue";
+import {ref} from "vue";
+
+const isValidLink = ref(true);
+
+
+</script>
+
+<template>
+  <!-- REPLACE -->
+  <div v-if="1>0">
+    <Banner/>
+    <LoginForm/>
+  </div>
+  <div v-else>
+    <NotFound/>
+  </div>
+</template>
---
+++ mineauth-frontend/src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Banner from "@/components/Banner.vue";
+</script>
+
+<template>
+  <Banner/>
+  <div class="centered-top">
+    <h1>Invalid Link!</h1>
+    <p>Your login link is invalid. Please log into the server and use '/login' to get your unique login link!</p>
+  </div>
+</template>
+
+<style scoped>
+.centered-top {
+  text-align: center;
+  margin-top: 10%;
+}
+
+h1 {
+  letter-spacing: 3px;
+  color: white;
+}
+
+p {
+  color: white;
+  font-size: x-large;
+}
+
+</style>
