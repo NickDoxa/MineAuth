01a89f2eeb71db5715de643014a353e9f615a728
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-02 18:23:12 -0400

    Added in server banners for users to customize the site page

---
+++ mineauth-frontend/src/components/ServerBanner.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+import {ref} from "vue";
+import axios from "axios";
+
+const bannerExists = ref(false)
+const imageUrl = ref("")
+getServerBanner()
+
+async function getServerBanner() {
+  await axios.get("http://localhost:8080/api/images/banner")
+      .then(async function (response) {
+        const data = response.data;
+        if (data.url !== undefined) {
+          bannerExists.value = true
+          imageUrl.value = data.url
+        }
+      }).catch(error => {
+        bannerExists.value = false
+        console.log("No banner found!")
+        console.log("Banner not found exception reason: " + error)
+      })
+}
+
+</script>
+
+<template>
+<div v-if="bannerExists" class="centered">
+  <img alt="Server Banner" :src="imageUrl" class="banner">
+</div>
+</template>
+
+<style scoped>
+.banner {
+  width: 35%;
+  height: auto;
+  text-align: center;
+  margin: auto;
+}
+
+.centered {
+  text-align: center;
+  color: white;
+  margin-top: 5%;
+}
+</style>
