4af118e3edbe1372c28becc6b62ea5f9f2d82a4b
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-02 22:24:25 -0400

    Added user profile component to show player models when logged in successfully

---
+++ mineauth-frontend/src/components/UserProfile.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref} from "vue";
+
+const props = defineProps(["uuid"])
+
+const imageSrc = ref("https://api.mineatar.io/body/full/")
+
+imageSrc.value = imageSrc.value + props.uuid;
+
+</script>
+
+<template>
+  <div class="centered-img">
+    <img alt="Player Skin Render" :src="imageSrc" class="img-banner">
+  </div>
+</template>
+
+<style scoped>
+
+.img-banner {
+  width: auto;
+  height: 30%;
+}
+
+.centered-img {
+  text-align: center;
+}
+
+</style>
